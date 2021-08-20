e09ef44c41ede9ffc8f62efc8ad094a2653f79d6
Andrew Sharkey <sharkand@hotmail.com>
2021-08-21 00:06:53 +0100

    First push to Github.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react';
+import Main from './components/Main';
+
 
 function App() {
+  const [councilsInfo, setCouncilsInfo] = useState(null);
+
+  useEffect(() => {
+  fetch("https://perioddignitydb-default-rtdb.europe-west1.firebasedatabase.app/.json")
+  .then(result => result.json())
+  .then(data => setCouncilsInfo(data))}, [] )
+
+  if (councilsInfo) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Main councilsInfo={councilsInfo}/>
+  )
+  };
+  return (
+    <h1>Loading...</h1>
+  )
 }
 
 export default App;
